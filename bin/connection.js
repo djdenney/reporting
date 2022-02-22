3aa083d2c6c3094516e03e194a8bcf5edc27f53b
djdenney <daniel@denneythe.dev>
2022-02-22 06:57:28 -0700

    added detailed query for testQuery

--- bin/connection.js
+++ bin/connection.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const knex = require("knex")({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_IP,
-        port: 3306,
-        user: process.env.DB_UID,
-        password: process.env.DB_KEY
-    }
+	client: "mysql",
+	connection: {
+		host: process.env.DB_IP,
+		port: 3306,
+		user: process.env.DB_UID,
+		password: process.env.DB_KEY,
+	},
 });
 
 module.exports = knex;
--- bin/gbg.js
+++ bin/gbg.js
@@ -1,19 +1,22 @@
 const knex = require("./connection");
 const tableName = "ORD_ORDER";
 const today = new Date(new Date(new Date().setDate(new Date().getDate())).toISOString().slice(0, 10));
-const yesterday = new Date(new Date(new Date().setDate(new Date().getDate() - 2)).toISOString().slice(0, 10));
+const yesterday = new Date(new Date(new Date().setDate(new Date().getDate() - 1)).toISOString().slice(0, 10));
 console.log(today);
 console.log(yesterday);
 
 async function testQuery() {
-    const response = await knex(tableName)
-        .withSchema("default_order")
-        .select("*")
-        .where("CREATED_TIMESTAMP", ">=", yesterday)
-        .andWhere("CREATED_TIMESTAMP", "<", today);
-    // const filtered = response.filter((order) => order.MAX_FULFILLMENT_STATUS_ID < "3000");
-    console.log(response.length)
-    return process.exit()
+	const response = await knex(tableName)
+		.withSchema("default_order")
+		.select("*")
+		.where("CREATED_TIMESTAMP", ">=", yesterday)
+		.andWhere("CREATED_TIMESTAMP", "<", today)
+		.andWhere({ IS_CONFIRMED: 1 })
+		.andWhere({ IS_CANCELLED: 0 })
+		.andWhere("ORDER_TOTAL", ">", "0");
+	// const filtered = response.filter((order) => order.MAX_FULFILLMENT_STATUS_ID >= "7000");
+	console.log(response.length);
+	return process.exit();
 }
 
-testQuery()
+testQuery();
